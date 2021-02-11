da5baf610ab00310115c834b91e4a777d3d39361
OB <obuyuktezgel@gmail.com>
2021-02-11 21:08:35 +0100

    Created restaurant index and added category to restaurant table

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,15 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :email, :phone, :category)
+  end
 end
