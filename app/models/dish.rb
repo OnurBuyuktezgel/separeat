c75826ccaa117be9b77561254e7a50d9196d1878
Malin Schadel <malin.schadel@gmail.com>
2021-02-12 19:23:20 +0100

    changed button and form styles

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,4 +1,6 @@
 class Dish < ApplicationRecord
   belongs_to :restaurant
   has_many :orders
+
+  CATEGORIES = ['Starter', 'Main Dish', 'Dessert', 'Soup', 'Salad', 'Fish', 'Meat', 'Pasta', 'Vegetarian', 'Vegan', 'Beverage', 'Hot Beverage', 'Alcoholic Beverage']
 end
