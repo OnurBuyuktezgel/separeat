9b2165ea0d1a7729ba7cc66bd7126d6962ff04d7
Ilona Dvorak <ilonab.dvorak@gmail.com>
2021-02-13 14:48:27 +0100

    added validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :dish
   belongs_to :visit
+
+enum status: { pending: 0, confirmed: 1, declined: 2 }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,12 @@ class User < ApplicationRecord
 
   has_many :restaurants
 
-  enum status: { pending: 0, confirmed: 1, declined: 2 }
+  USER_TYPES = ['Restaurant_owner', 'Customer']
+  validates :user_type, presence: true, inclusion: { in: USER_TYPES, allow_nil: false }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :address, presence: true
+  validates :phone, presence: true
+
 end
+
