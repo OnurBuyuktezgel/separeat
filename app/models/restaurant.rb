ba63c15cce40f918764b2078f58c5fab3cb47a31
Malin Schadel <malin.schadel@gmail.com>
2021-02-13 17:58:38 +0100

    added images to the seeds

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,8 +5,8 @@ class Restaurant < ApplicationRecord
   has_many :dishes
   has_one_attached :photo
 
-
   CATEGORIES = ['Asian', 'Bar', 'Brazilian', 'Burgers', 'Café', 'Chinese', 'Fish', 'Greek', 'Indian', 'Italian', 'Japanese', 'Moroccan', 'Mexican', 'Pasta', 'Polish', 'Spanish', 'Sushi', 'Thai', 'Turkish', 'Vegan', 'Vegetarian', 'Vietnamese']
+
   validates :category, inclusion: { in: CATEGORIES, allow_nil: false }
   validates :name, presence: true, uniqueness: true
   validates :address, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_13_121412) do
+ActiveRecord::Schema.define(version: 2021_02_13_143338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,7 +101,7 @@ ActiveRecord::Schema.define(version: 2021_02_13_121412) do
     t.string "last_name"
     t.string "address"
     t.string "phone"
-    t.integer "user_type"
+    t.string "user_type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
