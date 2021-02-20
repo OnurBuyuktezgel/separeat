a40a6a814b879bbefa3e6df9a1d60d5d19dccd55
OB <obuyuktezgel@gmail.com>
2021-02-20 13:23:00 +0100

    Added homepage, logo and made some css fixes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,8 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import { loadDynamicBannerText } from '../components/banner';
+
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -31,6 +33,7 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  loadDynamicBannerText();
 });
 
 import { Application } from "stimulus"
